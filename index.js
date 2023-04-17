65e8727b07a0f2492e3c48e44ec5600d704fed0d
rnpjroz <rnpjrau@gmail.com>
2023-04-17 10:50:00 +1000

    Changes after installing the SDK & running initial typescript code
    Changes after installing the SDK & running initial typescript code

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var msg = 'Hello World';
-console.log(msg);
+//initialise SDK
+// import { LeapHostSdkFactory } from '@leapdev/leap-host';
+// const sdk = LeapHostSdkFactory.getInstance();
+// if (!!sdk) {
+//     //this is the Sandbox ClientID, not the Live one yet
+//     sdk.init('PJOYCRM8LHYMHXNV').then(() => {
+//     //you can start calling APIs
+//     console.log(sdk.leapContext);
+//   });
+// }
+//-------------------------------------
+//just a sample server-side code
+var message = 'Hello, World!';
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+console.log(message);
